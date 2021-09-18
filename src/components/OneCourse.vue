4c0603fabe6b20ae231d929474fad1f72ef47fb6
KHELIFA Yassine <prostam27@gmail.com>
2021-09-18 23:38:04 +0200

    Props Input Parent To Child update

---
+++ src/components/OneCourse.vue
@@ -0,0 +1,19 @@
+<template >
+  
+    <div class="card my-3">
+        <img class="card-img-top" :src="course.image" :alt="course.title">
+        <div class="card-body">
+            <h4 class="card-title">{{  course.title }}</h4>
+            <p class="card-text">Text</p>
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    props: ['course']
+}
+</script>
+<style lang="">
+    
+</style>
